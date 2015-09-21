91c1090dfca17176e44f7556323a4823701ba34a
Christian Wortmann <cwortm14@student.aau.dk>
2015-09-21 15:07:18 +0200

    Revert "Revert "Test commit""
    This reverts commit 1d407eaf011847ff021b0d6c4e1af4eee45c5685.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	string Donner;
+	cin >> Donner;
+	cout << Donner;
+	return 0;
+}
+
